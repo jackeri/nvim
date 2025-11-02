36f5b0d951d8e770a54c8c6610ada7f680fdc2df
Jacker <shogaiam@gmail.com>
2025-11-02 23:45:57 +0200

    setup a way to close unused buffers on bulk with '<Leader>cub'

--- init.lua
+++ init.lua
@@ -1735,6 +1735,43 @@ vim.keymap.set('n', '<leader>fn', ':let @+ = expand("%:t")<CR>', { desc = 'Copy 
 --   end
 -- end)
 
+local function setup_close_unused_buffers()
+  local id = vim.api.nvim_create_augroup('startup', {
+    clear = false,
+  })
+
+  local persistbuffer = function(bufnr)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
+    vim.fn.setbufvar(bufnr, 'bufpersist', 1)
+  end
+
+  vim.api.nvim_create_autocmd({ 'BufRead' }, {
+    group = id,
+    pattern = { '*' },
+    callback = function()
+      vim.api.nvim_create_autocmd({ 'InsertEnter', 'BufModifiedSet' }, {
+        buffer = 0,
+        once = true,
+        callback = function()
+          persistbuffer()
+        end,
+      })
+    end,
+  })
+
+  vim.keymap.set('n', '<Leader>cub', function()
+    local curbufnr = vim.api.nvim_get_current_buf()
+    local buflist = vim.api.nvim_list_bufs()
+    for _, bufnr in ipairs(buflist) do
+      if vim.bo[bufnr].buflisted and bufnr ~= curbufnr and (vim.fn.getbufvar(bufnr, 'bufpersist') ~= 1) then
+        vim.cmd('bd ' .. tostring(bufnr))
+      end
+    end
+  end, { silent = true, desc = '[C]close [u]nused [b]uffers' })
+end
+
+setup_close_unused_buffers()
+
 if vim.g.neovide then
   -- https://neovide.dev/configuration.html
   -- NOTE: This is a workaround for the issue with macOS where the working directory is `/` at startup
